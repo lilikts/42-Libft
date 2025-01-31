8b1c03b826af7629b5e03e999c5c33fbc8c6dec3
Lilli Kloters <lkloters@3-F-13.42heilbronn.de>
2025-01-31 14:39:46 +0100

    again gnl

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lkloters <lkloters@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 09:58:15 by lkloters          #+#    #+#             */
-/*   Updated: 2025/01/31 14:27:47 by lkloters         ###   ########.fr       */
+/*   Updated: 2025/01/31 14:39:06 by lkloters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ char		*get_next_line(int fd);
 char		*ft_strchr(const char *s, int c);
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcpy(char *dest, const char *src, size_t dstsize);
-char		*join_strings(char const *s1, char const *s2);
+char		*ft_strjoin(char const *s1, char const *s2);
 
 #endif
