d7fbc4caf2b86a68644b3326a6fa0e6679d462d9
Lilli Kloters <lkloters@3-F-13.42heilbronn.de>
2025-02-04 15:11:23 +0100

    changed flood_fill_target

--- libft/flood_fill_target.c
+++ libft/flood_fill_target.c
@@ -6,7 +6,7 @@
 /*   By: lkloters <lkloters@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 14:55:54 by lkloters          #+#    #+#             */
-/*   Updated: 2025/01/09 14:38:56 by lkloters         ###   ########.fr       */
+/*   Updated: 2025/02/04 15:10:11 by lkloters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 void	flood_fill_target(char **map, int x, int y, char target)
 {
-	if (x < 0 || y < 0 || !map[y] || map[y][x] == '\0' || map[y][x] == target)
-		return ;
-	if (map[y][x] == 'F')
+	if (x < 0 || y < 0 || !map[y] || map[y][x] == '\0' || map[y][x] == target || map[y][x] == 'F')
 		return ;
 	map[y][x] = 'F';
 	flood_fill(map, x + 1, y, target);
